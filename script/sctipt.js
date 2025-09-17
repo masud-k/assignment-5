b964ca9fdcf251c88dc0ebeff0b1465ec8cd3d6d
mostafiz-devOps <mostafizurofficial@outlook.com>
2025-09-17 22:06:27 +0600

    js started

--- script/sctipt.js
+++ script/sctipt.js
@@ -1,8 +1,2 @@
 // if clicked buy tickert button then go to seat plan and number area.
 
-function goToSeatPlan() {
-    const buyTicketBtn = document.getElementById("buy-ticket");
-    buyTicketBtn.addEventListener("click", function() {
-        window.location.href = "#seat-plan-and-number";
-    });
-}
