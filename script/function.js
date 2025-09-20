753567082edfcb7833f9d9761e3c1cabc115fbb8
mostafiz-devOps <mostafizurofficial@outlook.com>
2025-09-20 22:07:38 +0600

    nothing else

--- script/function.js
+++ script/function.js
@@ -9,9 +9,11 @@ buyBtn.addEventListener("click", function () {
 
 const ticketSelection = document.querySelectorAll(".seat.bg-slate-300");
 const seatDetails = document.querySelector(".bg-white.p-6"); // Seat details div
+const selectedSeats = [];
 
 for (let i=0; i<ticketSelection.length; i++) {
     ticketSelection[i].addEventListener("click", function () {
+        const seatNumber = this.innerText;
         if (this.classList.contains("bg-slate-300")) {
             this.classList.remove("bg-slate-300");
             this.classList.add("bg-green-400");
